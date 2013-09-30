85e7ea6a3cbaeecd24a1168243be811c2d4e8414
MasDennis <ippeldv@gmail.com>
2013-09-30 22:26:28 +0100

    Merged @chegewara cloud recognition code. Also added all the markers because some people didn't know where to download them from.

--- RajawaliVuforiaExample/src/com/example/rajawalivuforiaexample/RajawaliVuforiaExampleActivity.java
+++ RajawaliVuforiaExample/src/com/example/rajawalivuforiaexample/RajawaliVuforiaExampleActivity.java
@@ -3,13 +3,19 @@ package com.example.rajawalivuforiaexample;
 import rajawali.util.RajLog;
 import rajawali.vuforia.RajawaliVuforiaActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
 
 public class RajawaliVuforiaExampleActivity extends RajawaliVuforiaActivity {
 	private RajawaliVuforiaExampleRenderer mRenderer;
+	private RajawaliVuforiaActivity mUILayout;
+    private Button mStartScanButton;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setCloudRecoDatabase("a75960aa97c3b72a76eb997f9e40d210d5e40bf2", "aac883379f691a2550e80767ccd445ffbaa520ca");
 		startVuforia();
 	}
 
@@ -41,5 +47,31 @@ public class RajawaliVuforiaExampleActivity extends RajawaliVuforiaActivity {
 		mRenderer = new RajawaliVuforiaExampleRenderer(this);
 		mRenderer.setSurfaceView(mSurfaceView);
 		super.setRenderer(mRenderer);
+		
+	    //Add button for Cloud Reco:
+        mStartScanButton = new Button(this);
+        mStartScanButton.setText("Start Scanning CloudReco");
+         
+        mStartScanButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    enterScanningModeNative();
+                     mStartScanButton.setVisibility(View.GONE);
+                 }
+        });
+        
+        mUILayout = this;
+        mUILayout.addContentView(mStartScanButton, 
+            new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+     
 	}
+	
+    public void showStartScanButton()
+    {
+        this.runOnUiThread(new Runnable() {
+                public void run() {
+                    if  (mStartScanButton != null)
+                        mStartScanButton.setVisibility(View.VISIBLE);
+                 }
+         });
+    }    
 }
